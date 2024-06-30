f26e43ff8bc478d486925f0dc07e95bdf102c0ce
Filip Razvan <filip.razvan@agilefreaks.com>
2024-06-30 13:10:55 +0300

    Set dummy output handler from view

--- api/ai_assistant/views.py
+++ api/ai_assistant/views.py
@@ -2,8 +2,12 @@ from django.http import JsonResponse
 from handbook_llm_wrapper.main import create_assistant, ask_question
 import os
 
+def streaming_handler_function(token):
+    print(f"--received token: ({token})----")
+
 MODEL_PATH = os.path.expanduser('~/llms/ggml-model-Q4_1.gguf')
 assistant = create_assistant(MODEL_PATH)
+assistant.set_streaming_handler_function(streaming_handler_function)
 
 def ask(request):
     question = request.GET.get('question')
